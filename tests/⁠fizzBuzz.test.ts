2bf8a6a3ae7dc1cd6b633c328f29cd0993d24834
dakeinu <remi.falcati@ynov.com>
2024-06-25 11:17:43 +0200

    Failing Test Multiples of 3

--- tests/⁠fizzBuzz.test.ts
+++ tests/⁠fizzBuzz.test.ts
@@ -3,3 +3,7 @@ import { fizzBuzz } from '../src/fizzBuzz';
 test('returns number as string', () => {
   expect(fizzBuzz(1)).toBe('1');
 });
+
+test('returns Fizz for multiples of 3', () => {
+  expect(fizzBuzz(3)).toBe('Fizz');
+});
